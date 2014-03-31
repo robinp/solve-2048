749fd1ad7e6164f7ca72f6392f0d2c068866c4ca
Robin Palotai <palotai.robin@gmail.com>
2014-03-31 05:23:22 +0000

    Added deadline for computation

--- Main.hs
+++ Main.hs
@@ -1,9 +1,13 @@
 module Main where
 
+import Control.Monad
+import Data.Foldable (foldl')
 import qualified Data.List as L
+import Data.Time.Clock
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Maybe
+import Debug.Trace
 
 -- * Representation
 
@@ -16,6 +20,9 @@ newtype Table = Table { unTable :: [[Maybe Power]] }
 instance Show Table where
   show (Table rows) = L.intercalate "\n" (map showRow rows) ++ "\n"
 showRow = concat . map (("\t" ++) . maybe "_" show)
+
+containsPower (Table rows) p =
+  any (any (== Just p)) rows 
 
 data Dir = L | R | U | D
 
@@ -70,6 +77,54 @@ nexts (Table rows) =
       _       -> []
 
 -- * Heuristics
+
+currentTime :: IO DiffTime
+currentTime = fmap utctDayTime getCurrentTime
+
+evalDepth 
+  :: Int  -- ^ Depth remaining
+  -> Table
+  -> (Table -> IO (Maybe a))  -- ^ Heuristic (time-capped)
+  -> (Table -> a)  -- ^ Heuristics for final state
+  -> ([Maybe a] -> Maybe a) -- ^ Heuristic combiner
+  -> IO (Maybe a)
+evalDepth d t f g c = go d t
+  where
+  go 0 t = f t
+  go d t = 
+    let nts = nexts t  -- TODO use version having directions here
+    in if null nts
+       then return . Just . g $ t
+       else fmap c . sequence . map (go (d - 1)) $ nts
+
+withDeadline
+  :: (a -> b) 
+  -> DiffTime  -- ^ Absolute deadline
+  -> a -> IO (Maybe b)
+withDeadline f deadline a = do
+  current_t <- currentTime
+  return $ if current_t < deadline
+    then Just (f a)
+    else Nothing
+
+heur1 :: Table -> Double
+heur1 t = 0.0
+
+worst :: [Maybe Double] -> Maybe Double
+worst ms = trace (show ms) $ case catMaybes ms of
+  [] -> Nothing
+  x:xs -> Just (foldl' min x xs)
+
+targetPower = 11
+
+final :: Table -> Double
+final t = if t `containsPower` targetPower then 99999 else -99999
+
+eval1 :: Int -> Table -> IO (Maybe Double) 
+eval1 d t = do
+  current_t <- currentTime
+  let deadline = current_t + secondsToDiffTime 1
+  evalDepth d t (withDeadline heur1 deadline) final worst 
 
 -- * Parse and main
 
